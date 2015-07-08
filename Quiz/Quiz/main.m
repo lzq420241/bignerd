2257b571e3753de5fdbc22a87f421daa9d632d2a
liziqiang <lzq.hdu@gmail.com>
2015-07-08 23:37:52 +0800

    add icon, but still has issue about layout. simply follow 3rd edition of iOS programming: the big nerd ranch guide. chapter 1.

---
+++ Quiz/Quiz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by LiZiqiang on 7/7/15.
+//  Copyright (c) 2015 LiZiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
